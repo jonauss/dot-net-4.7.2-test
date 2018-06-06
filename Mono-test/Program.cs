684d63da35fec787349be1b34a5463a6d3e3753a
Jonas Ausevicius <ausevicius@gmail.com>
2018-06-06 10:09:26 +0300

    Initial commit.

---
+++ Mono-test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Mono_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DSA dsa = DSA.Create(2048))
+            {
+                Console.WriteLine("This is a .NET 4.7.2 Application!");
+                Console.WriteLine(dsa.GetHashCode());
+            }
+        }
+    }
+}
